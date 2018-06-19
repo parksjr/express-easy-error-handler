c66f328966cc3093a8a75c94e4436445ef4eb82b
Michael Parks <mparks@acponline.org>
2018-06-19 09:50:29 -0400

    added prepublish script

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import HttpError from './models/error.http';
 import {ErrorHandler, NotFoundHandler} from './middleware/error-handler';
 
 export default HttpError;
-export {ErrorHandler};
-export {NotFoundHandler};
+export {ErrorHandler, NotFoundHandler};
